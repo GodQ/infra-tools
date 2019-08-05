d34471beca0868c334354c526fcaa8245dd9cb7a
chuanhaoq <chuanhaoq@vmware.com>
2019-08-05 10:57:11 +0800

    add type convert in logstash config

--- elk/manual/add_log.sh
+++ elk/manual/add_log.sh
@@ -1,1 +1,1 @@
-echo '{"log_id":"01","info":"[01-2019]我是第一条信息","logDate":"2019-1-16 17:39:31"}' >> /var/log/test.log
+echo '{"log_id":"01","fail_rate":"0.1", "success_rate": 0.9, "info":"[01-2019]我是第一条信息","logDate":"2019-1-16 17:39:31"}' >> /var/log/test.log
