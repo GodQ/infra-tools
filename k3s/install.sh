cdaaa012b1424f550cd1a7367dfd9ca6f73e1415
GodQ <quchuanhao@gmail.com>
2019-09-01 23:33:18 +0800

    add k3s

---
+++ k3s/install.sh
@@ -0,0 +1,3 @@
+export INSTALL_K3S_EXEC="--docker --disable-agent"
+
+bash install-k3s.sh
---
+++ k3s/start_agent.sh
@@ -0,0 +1,1 @@
+k3s agent --server https://127.0.0.1:6443 --token K1048a2f3d0123f424ed6fb0f18ca5869f13ae156229320ccaca3c4783e5ce45ff7::node:ffd3ac9cc82b5a05008fea51b03780f1 --docker
